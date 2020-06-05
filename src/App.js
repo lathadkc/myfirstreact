be167348963b53b6d066ea6e01f1b77b1d1c7f5b
lathadkc <lathakrishnasamy2000@gmail.com>
2020-06-05 23:22:54 +0530

    added arrays

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import HookCounter from './components/HookCounter';
 import HookcounterThree from './components/HookCounterThree';
+import HookCounter4 from './components/HookCounter4';
 
 function App() {
   return (
     <div className="App">
       <HookCounter></HookCounter>
     <HookcounterThree></HookcounterThree>
+    <HookCounter4></HookCounter4>
         latha
     </div>
   );
---
+++ src/components/HookCounter4.js
@@ -0,0 +1,22 @@
+import React, { useState }  from 'react';
+
+function HookCounter4() {
+    const [items,setItems] = useState([])
+    const hc = () => {
+        setItems([...items,{id : items.length, value : items.length+100}]);
+    }
+    return(
+<div>
+<button  onClick = {hc} >Add A Number</button>
+<ul>
+{
+items.map(item => (
+    <li key = {item.id} >{item.value}</li>
+    
+))
+}
+</ul>
+</div>
+    );
+}
+export default HookCounter4;
