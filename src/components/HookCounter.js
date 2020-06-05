b630682d81ef873d0b76e6679c19249eeff86796
lathadkc <lathakrishnasamy2000@gmail.com>
2020-06-05 22:07:51 +0530

    added incrementfive

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -4,7 +4,12 @@ import React from 'react';
  function HookCounter() {
     const [count,setCount] = useState(0);
 
-    
+    const incrementFive = () => {
+        for (let i = 0; i < 5; i++) {
+            setCount(oc => oc+1);
+            
+        }
+    }
         return (
              <div >
                  <table >
@@ -13,6 +18,7 @@ import React from 'react';
 <tr>   <td><button onClick = {() => setCount(count +1)}  >increment</button></td>
 <td><button onClick = {() => setCount(count -1)}  >Decrement</button></td>
 <td><button onClick = {() => setCount(0)}  >Reset</button></td>
+<td><button onClick = {incrementFive}  >IncrementFive</button></td>
  </tr>
 
                  </table>
