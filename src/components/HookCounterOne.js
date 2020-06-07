39f44ae32561b0569f11057351c00feabe47bd94
lathadkc <lathakrishnasamy2000@gmail.com>
2020-06-07 11:40:19 +0530

    added useeffect watch for only count changes and not for name

--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -3,13 +3,15 @@ import React, { useState, useEffect } from 'react';
 
 function HookCounterOne() {
     const[count,setCount] = useState(0);
+    const [name, setName] = useState('');
+
     useEffect(() =>{
         console.log("within useEffectHookCounterONe");
         document.title = `you clicked ${count} times`;
-    });
+    },[count]);
     return(
 <div>
-
+<input type = "text" onChange = {(e) => setName(e.target.value)}></input>
 <button onClick = {() => setCount(count+1)} >Click {count}times</button>
 </div>
 
