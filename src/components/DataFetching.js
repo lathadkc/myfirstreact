1928204d8d5ce3336383bbd02e3eb59ae0441b37
lathadkc <lathakrishnasamy2000@gmail.com>
2020-06-08 11:48:36 +0530

    installed axios

---
+++ src/components/DataFetching.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const DataFetching = () => {
+    const [posts, setPosts] = useState([])
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DataFetching
