9594162ff919d97803ac9ee237db44e75ad60694
lathadkc <lathakrishnasamy2000@gmail.com>
2020-08-25 18:36:01 +0530

    commit DataFetchng1

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const DataFetching = () => {
-    const [posts, setPosts] = useState([])
-    
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+            console.log(res);
+            setPosts(res.data);
+        }
+        ).catch((err) => {
+            console.log(err);
+            
+        }
+        );
+        return () => {
+            
+        }
+    }, [])
     return (
         <div>
-            
+            <ul>
+                {
+                    posts.map(ost => (
+                    <li key = {ost.id}>{ost.title}</li>
+                    )
+                    )
+                }
+            </ul>
         </div>
     )
 }
